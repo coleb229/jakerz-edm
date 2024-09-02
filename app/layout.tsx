e56efe35ab20bfffbd466b42f57395901a0c89b5
Cole Brant <cbrant229@gmail.com>
2024-09-02 03:04:47 -0500

    setup the basic project structure with necessary dependencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,17 @@
 import './globals.css'
+import '@mantine/core/styles.css';
+import '@mantine/dates/styles.css';
+import '@mantine/dropzone/styles.css';
+import { createTheme, MantineProvider, ColorSchemeScript } from '@mantine/core'
 
 export const metadata = {
   title: 'Next.js',
   description: 'AWS S3 Upload Example',
 }
+
+const theme = createTheme({
+  /** Put your mantine theme override here */
+});
 
 export default function RootLayout({
   children,
@@ -12,7 +20,21 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <head>
+        <meta charSet="UTF-8" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0"
+        />
+        <title>My awesome app</title>
+
+        <ColorSchemeScript />
+      </head>
+        <body>
+          <MantineProvider theme={theme}>
+            {children}
+          </MantineProvider>
+        </body>
     </html>
   )
 }
