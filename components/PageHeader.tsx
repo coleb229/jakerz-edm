27e4fad05f93b10774053298f7d4f017df4e3883
Cole Brant <coleb@Coles-Laptop.lan>
2024-09-11 12:41:18 -0500

    added header icon

--- components/PageHeader.tsx
+++ components/PageHeader.tsx
@@ -1,12 +1,24 @@
+'use client'
 import { Text } from '@mantine/core';
 import styles from './PageHeader.module.css';
+import Image from 'next/image';
+import JakerzIcon from '@/public/jakerz-icon.png';
 
 export const PageHeader = ({ layout }:any) => {
   return (
     <div className={styles.container}>
-      <div>
+      <div className={styles.header}>
         <Text size="xl" className={styles.headerText}>{layout.headerText}</Text>
         <Text size="md" className={styles.subheader}>{layout.subheaderText}</Text>
+      </div>
+      <div>
+        <Image
+          src={layout.headerImage !== undefined || '' ? layout.headerImage : JakerzIcon}
+          className={styles.headerImage}
+          alt="Header Image"
+          width={100}
+          height={100}
+        />
       </div>
     </div>
   );
