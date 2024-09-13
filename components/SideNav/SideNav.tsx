1c5fd58a4523afd9d12fd2c6be7dec2436efadd8
Cole Brant <coleb@Coles-Laptop.lan>
2024-09-13 01:42:00 -0500

    added animation to side nav open/close

--- components/SideNav/SideNav.tsx
+++ components/SideNav/SideNav.tsx
@@ -13,10 +13,10 @@ import styles from './SideNav.module.css';
 export const SideNav = ({ session }:any) => {
   const [opened, { toggle }] = useDisclosure();
   return (
-    <div className={'fixed top-0 left-0 bg-slate-800 h-full p-4 flex flex-col items-center ' + styles.container}>
-      <Burger opened={opened} onClick={toggle} aria-label="Toggle navigation" size='lg' color='white' className={styles.navLink} />
+    <div className={`fixed top-0 left-0 bg-slate-800 h-full p-4 flex flex-col items-center ${styles.container} ${opened ? styles.open : ''}`}>
+      <Burger opened={opened} ml='auto' onClick={toggle} aria-label="Toggle navigation" size='lg' color='white' className={styles.navLink} />
       {opened ? <OpenNav /> : <ClosedNav />}
-      <div className='items-center'>
+      <div className={`flex-1 ${opened ? styles.open : ''}`}>
         {session ? (
           <NavLink
             className={styles.navLinkChild}  
